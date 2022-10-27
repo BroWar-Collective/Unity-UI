24cf59ac9222744bf38e15fa1c513b964d265789
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2022-10-27 22:26:10 +0200

    Basic UiButton implementation

--- Assets/Package/Runtime/UiButton.cs
+++ Assets/Package/Runtime/UiButton.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace BroWar.UI
@@ -8,5 +10,37 @@ namespace BroWar.UI
     /// </summary>
     [RequireComponent(typeof(Button))]
     public class UiButton : UiObject
-    { }
+    {
+        [SerializeField, NotNull]
+        private Button button;
+        [SerializeField]
+        private Image icon;
+        [SerializeField]
+        private TextMeshProUGUI label;
+
+        public event Action OnClicked;
+
+        public bool HasLabel => label != null;
+
+        public string Label
+        {
+            get => label.text;
+            set => label.text = value;
+        }
+
+        private void Awake()
+        {
+            if (button == null)
+            {
+                button = GetComponent<Button>();
+            }
+
+            button.onClick.AddListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            OnClicked?.Invoke();
+        }
+    }
 }
