656d5f35e5dcc782863327cd1b55bc1300a78a44
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2022-10-27 22:33:40 +0200

    Implement IViewsManager & DefaultViewsManager

--- Runtime/IViewsManager.cs
+++ Runtime/IViewsManager.cs
@@ -1,5 +1,23 @@
-﻿namespace BroWar.UI
+﻿using System;
+using System.Collections.Generic;
+
+namespace BroWar.UI
 {
+    /// <summary>
+    /// TODO
+    /// </summary>
     public interface IViewsManager
-    { }
+    {
+        void Show<T>() where T : UiView;
+        void Show(Type viewType);
+        void Hide<T>() where T : UiView;
+        void Hide(Type viewType);
+        bool TryGetView(Type type, out UiView view);
+        bool TryGetView<T>(out T view) where T : UiView;
+        void HideAll();
+        void HideActiveViews();
+
+        IReadOnlyCollection<UiView> Views { get; }
+        IReadOnlyCollection<UiView> ActiveViews { get; }
+    }
 }
