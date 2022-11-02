76f3688471cba4876a69503f085ab706b160dddc
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2022-11-02 23:37:47 +0100

    Minor refactor changes

--- Assets/Package/Runtime/Management/UiHandlersManager.cs
+++ Assets/Package/Runtime/Management/UiHandlersManager.cs
@@ -1,7 +1,12 @@
-﻿namespace BroWar.UI.Management
+﻿using UnityEngine;
+
+namespace BroWar.UI.Management
 {
     public class UiHandlersManager
     {
+        [SerializeField, SerializeReference, ReferencePicker]
+        private IUiFeatureHandler[] handlers;
+
         //TODO: inject handlers
     }
 }
