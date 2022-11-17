c6fcc4133dd2f9363cb761af534b86edcab6504b
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2022-11-18 00:06:22 +0100

    Function tooltip system for UI elements

--- Runtime/Handlers/TooltipHandler.cs
+++ Runtime/Handlers/TooltipHandler.cs
@@ -1,29 +1,79 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace BroWar.UI.Handlers
 {
     using BroWar.UI.Tooltip;
-
-    //https://www.youtube.com/watch?v=HXFoUGw7eKk&t=227s&ab_channel=GameDevGuide
 
     [AddComponentMenu("BroWar/UI/Handler/Tooltip Handler")]
     public class TooltipHandler : UiHandlerBehaviour, ITooltipHandler
     {
         [SerializeField]
         private Canvas canvas;
+        [SerializeField, NotNull]
+        private TooltipBehaviour tooltipPrefab;
+        [SerializeField, Disable]
+        [Tooltip("Tooltip instance created in runtime.")]
+        private TooltipBehaviour tooltip;
 
         //TODO: temporary
+        private void OnEnable()
+        {
+            Prepare();
+        }
+
         private void Update()
         {
             Tick();
         }
 
-        public override void Tick()
+        private void OnDisable()
         {
-            //TODO: update tooltips
+            Dispose();
         }
 
+        public override void Prepare()
+        {
+            base.Prepare();
+            tooltip = Instantiate(tooltipPrefab, CanvasParent);
+            tooltip.name = tooltipPrefab.name;
+            HideTooltip();
+        }
+
+        public override void Tick()
+        {
+            if (!IsTooltipActive)
+            {
+                return;
+            }
+
+            var position = Mouse.current.position.ReadValue();
+            tooltip.UpdateRect(position);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            HideTooltip();
+        }
+
+        public void ShowTooltip(string contentText)
+        {
+            tooltip.UpdateText(contentText);
+            tooltip.Show();
+        }
+
+        public void HideTooltip()
+        {
+            if (tooltip != null && tooltip.IsActive)
+            {
+                tooltip.Hide();
+            }
+        }
+
+        private Transform CanvasParent => canvas.transform;
+
         public override bool IsTickable => IsTooltipActive;
-        public bool IsTooltipActive { get; private set; }
+        public bool IsTooltipActive => tooltip != null && tooltip.IsActive;
     }
 }
--- Runtime/Tooltip/TooltipTarget.cs
+++ Runtime/Tooltip/TooltipTarget.cs
@@ -1,14 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Zenject;
 
 namespace BroWar.UI.Tooltip
 {
+    /// <summary>
+    /// Component responsible for showing/hiding tooltips on an associated UI object.
+    /// </summary>
     [DisallowMultipleComponent]
     [AddComponentMenu("BroWar/UI/Tooltip/Tooltip Target")]
-    public class TooltipTarget : MonoBehaviour
+    public class TooltipTarget : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
+        [SerializeField, TextArea(4, 8)]
+        private string tooltipContent;
+        [SerializeField, Min(0)]
+        [Tooltip("Time needed to show tooltip content.")]
+        private float offsetTime = 0.0f;
+
         private ITooltipHandler tooltipHandler;
+        private Sequence sequence;
+
+        private void ShowTooltip()
+        {
+            tooltipHandler.ShowTooltip(tooltipContent);
+        }
+
+        private void HideTooltip()
+        {
+            tooltipHandler.HideTooltip();
+        }
 
         [Inject]
         internal void Inject(ITooltipHandler tooltipHandler)
@@ -16,9 +37,24 @@ namespace BroWar.UI.Tooltip
             this.tooltipHandler = tooltipHandler;
         }
 
-        //TODO: temporary
-        [Inject]
-        internal void Inject(List<IUiHandler> handlers)
-        { }
+        void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+        {
+            if (!ShouldShowContent)
+            {
+                return;
+            }
+
+            sequence = DOTween.Sequence();
+            sequence.AppendInterval(offsetTime);
+            sequence.AppendCallback(ShowTooltip);
+        }
+
+        void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+        {
+            sequence?.Kill();
+            HideTooltip();
+        }
+
+        private bool ShouldShowContent => !string.IsNullOrEmpty(tooltipContent);
     }
 }
