ffa3a04eed036c5f0cfd6c9810b4078e22f5ef20
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2023-05-10 22:15:22 +0200

    Add FadeIn/FadeOut animations

---
+++ Runtime/Animation/Animations/FadeInAnimationContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace BroWar.UI.Animation.Animations
+{
+    [Serializable]
+    public class FadeInAnimationContext : IAnimationContext
+    {
+        [SerializeField]
+        private CanvasGroup group;
+        [SerializeField, Min(0)]
+        private float duration = 1.0f;
+
+        public Sequence GetSequence()
+        {
+            var sequence = DOTween.Sequence();
+            sequence.Append(group.DOFade(1.0f, duration));
+            return sequence;
+        }
+    }
+}
