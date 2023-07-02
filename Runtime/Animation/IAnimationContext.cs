ce81df6422929c77701e70d2874b419bd7a94444
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2023-07-02 23:47:57 +0200

    Minor refactor changes

--- Runtime/Animation/IAnimationContext.cs
+++ Runtime/Animation/IAnimationContext.cs
@@ -4,7 +4,15 @@ namespace BroWar.UI.Animation
 {
     public interface IAnimationContext
     {
+        /// <summary>
+        /// Creates animation <see cref="Sequence"/>.
+        /// </summary>
+        /// <param name="fromSequence">Indicates if <see cref="Sequence"/> should define 'From' value.</param>
         Sequence GetSequence(bool fromSequence);
+        /// <summary>
+        /// Appends animation <see cref="Sequence"/>.
+        /// </summary>
+        /// <param name="fromSequence">Indicates if <see cref="Sequence"/> should define 'From' value.</param>
         Sequence GetSequence(bool fromSequence, Sequence sequence);
     }
 }
