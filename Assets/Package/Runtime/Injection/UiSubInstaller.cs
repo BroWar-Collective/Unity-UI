cfb360bb96021d1dea1b75e8320383c1b654bbe7
Miłosz Matkowski <milosz.matkowski96@gmail.com>
2023-12-23 22:38:05 +0100

    Add IUiManager binding

--- Assets/Package/Runtime/Injection/UiSubInstaller.cs
+++ Assets/Package/Runtime/Injection/UiSubInstaller.cs
@@ -19,6 +19,8 @@ namespace BroWar.UI.Injection
         protected override void OnInstall(DiContainer container)
         {
             Assert.IsNotNull(manager, $"[UI][Injection] {nameof(UiManager)} not available.");
+            container.Bind<IUiManager>()
+                .FromInstance(manager);
             var handlers = manager.Handlers;
             foreach (var handler in handlers)
             {
